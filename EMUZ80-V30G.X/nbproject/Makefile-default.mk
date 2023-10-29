0a79351b4ff1900e1e370c37169286a4f7f7c58a
Gazelle <148423174+Gazelle8087@users.noreply.github.com>
2023-10-29 21:05:51 +0900

    Add files via upload

--- EMUZ80-V30G.X/nbproject/Makefile-default.mk
+++ EMUZ80-V30G.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=emuz80-V30G.c
+SOURCEFILES_QUOTED_IF_SPACED=emuz80_V20G.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/emuz80-V30G.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/emuz80-V30G.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/emuz80_V20G.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/emuz80_V20G.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/emuz80-V30G.p1
+OBJECTFILES=${OBJECTDIR}/emuz80_V20G.p1
 
 # Source Files
-SOURCEFILES=emuz80-V30G.c
+SOURCEFILES=emuz80_V20G.c
 
 
 
@@ -94,22 +94,22 @@ MP_PROCESSOR_OPTION=18F47Q84
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/emuz80-V30G.p1: emuz80-V30G.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/emuz80_V20G.p1: emuz80_V20G.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/emuz80-V30G.p1.d 
-	@${RM} ${OBJECTDIR}/emuz80-V30G.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=snap   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O2 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/emuz80-V30G.p1 emuz80-V30G.c 
-	@-${MV} ${OBJECTDIR}/emuz80-V30G.d ${OBJECTDIR}/emuz80-V30G.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/emuz80-V30G.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/emuz80_V20G.p1.d 
+	@${RM} ${OBJECTDIR}/emuz80_V20G.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=snap   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O2 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/emuz80_V20G.p1 emuz80_V20G.c 
+	@-${MV} ${OBJECTDIR}/emuz80_V20G.d ${OBJECTDIR}/emuz80_V20G.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/emuz80_V20G.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
-${OBJECTDIR}/emuz80-V30G.p1: emuz80-V30G.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/emuz80_V20G.p1: emuz80_V20G.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/emuz80-V30G.p1.d 
-	@${RM} ${OBJECTDIR}/emuz80-V30G.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O2 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/emuz80-V30G.p1 emuz80-V30G.c 
-	@-${MV} ${OBJECTDIR}/emuz80-V30G.d ${OBJECTDIR}/emuz80-V30G.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/emuz80-V30G.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/emuz80_V20G.p1.d 
+	@${RM} ${OBJECTDIR}/emuz80_V20G.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O2 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/emuz80_V20G.p1 emuz80_V20G.c 
+	@-${MV} ${OBJECTDIR}/emuz80_V20G.d ${OBJECTDIR}/emuz80_V20G.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/emuz80_V20G.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
